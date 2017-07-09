8eee576e9adba254de22704ce351bbe65794a060
J2ghz <j2.00ghz@gmail.com>
2017-07-09 10:51:47 +0200

    Add project files.

---
+++ xxHash64Bench/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xxHash64Bench
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
