80989e866545f5401e9a0cf3c01bf472defa273c
J2ghz <j2.00ghz@gmail.com>
2017-09-14 21:25:00 +0200

    redo

--- xxHash64Bench/BenchmarkDotNet.Artifacts/results/HashBench-report-github.md
+++ xxHash64Bench/BenchmarkDotNet.Artifacts/results/HashBench-report-github.md
@@ -1,20 +1,15 @@
 ``` ini
 
-BenchmarkDotNet=v0.10.8, OS=Windows 10.0.16232
-Processor=Intel Core i7-4710HQ CPU 2.50GHz (Haswell), ProcessorCount=8
-Frequency=2435765 Hz, Resolution=410.5486 ns, Timer=TSC
-dotnet cli version=2.0.0-preview2-006497
+BenchmarkDotNet=v0.10.8, OS=Windows 10 Redstone 2 (10.0.15063)
+Processor=Intel Core i7-3770 CPU 3.40GHz (Ivy Bridge), ProcessorCount=8
+Frequency=3312794 Hz, Resolution=301.8600 ns, Timer=TSC
+dotnet cli version=2.0.1-servicing-006933
   [Host]     : .NET Core 4.6.25211.01, 64bit RyuJIT
   DefaultJob : .NET Core 4.6.25211.01, 64bit RyuJIT
 
 
 ```
- |   Method | dataSizeBytes |             Mean |         Error |        StdDev |
- |--------- |-------------- |-----------------:|--------------:|--------------:|
- | **xxHash64** |          **1073** |         **2.215 us** |     **0.0078 us** |     **0.0073 us** |
- | **xxHash64** |         **10737** |        **19.021 us** |     **0.0625 us** |     **0.0585 us** |
- | **xxHash64** |        **107374** |       **186.177 us** |     **0.7968 us** |     **0.7453 us** |
- | **xxHash64** |       **1073741** |     **1,849.961 us** |     **7.2826 us** |     **6.8121 us** |
- | **xxHash64** |      **10737418** |    **18,621.881 us** |    **62.7286 us** |    **58.6764 us** |
- | **xxHash64** |     **107374182** |   **186,785.877 us** |   **614.1825 us** |   **574.5067 us** |
- | **xxHash64** |    **1073741824** | **1,859,136.249 us** | **1,594.9764 us** | **1,331.8774 us** |
+ | Method |     Mean |     Error |    StdDev |
+ |------- |---------:|----------:|----------:|
+ | FileIO | 4.893 ms | 0.0768 ms | 0.0681 ms |
+ |    Mmf | 2.999 ms | 0.0598 ms | 0.0857 ms |
--- xxHash64Bench/Program.cs
+++ xxHash64Bench/Program.cs
@@ -4,35 +4,42 @@ using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using System.Data.HashFunction;
 using System.Threading.Tasks;
+using System.IO.MemoryMappedFiles;
+using System.IO;
 
 namespace xxHash64Bench
 {
-    public class HashBench
-    {
-        [Params(1073, 10737, 107374, 1073741, 10737418, 107374182, 1073741824)]
-        public int dataSizeBytes { get; set; }
-
-        private byte[] data;
-
-        private readonly xxHash xxH64 = new xxHash(64);
-
-        [GlobalSetup]
-        public void GlobalSetup()
-        {
-            data = new byte[dataSizeBytes];
-            new Random(42).NextBytes(data);
-        }
-
-        [Benchmark]
-        public byte[] xxHash64() => xxH64.ComputeHash(data).Hash;
-    }
-
     public static class Program
     {
         public static void Main(string[] args)
         {
             var summary = BenchmarkRunner.Run<HashBench>();
             Console.ReadKey();
+        }
+    }
+
+    public class HashBench
+    {
+        private const string path = @"D:\hashed2\1D-E9-39-0D-F2-FD-6D-F4";
+        private readonly xxHash xxH64 = new xxHash(64);
+
+        [Benchmark]
+        public async Task<byte[]> FileIO()
+        {
+            using (var stream = new FileInfo(path).Open(FileMode.Open))
+            {
+                return await this.xxH64.ComputeHashAsync(stream);
+            }
+        }
+
+        [Benchmark]
+        public async Task<byte[]> Mmf()
+        {
+            using (var file = MemoryMappedFile.CreateFromFile(path))
+            using (var stream = file.CreateViewStream())
+            {
+                return await this.xxH64.ComputeHashAsync(stream);
+            }
         }
     }
 }
